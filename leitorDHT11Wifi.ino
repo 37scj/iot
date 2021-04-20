68a674c03521acee3c3f9ed559b7dde682e59251
Lucas Rodrigues do Nascimento <lucasrodriguesdonascimento@outlook.com>
2021-04-20 20:17:11 -0300

    Update leitorDHT11Wifi.ino

--- leitorDHT11Wifi.ino
+++ leitorDHT11Wifi.ino
@@ -1,11 +1,22 @@
+// lib do DHT11
 #include <DHT.h>
-
+// Lib do telegram
+#include <UniversalTelegramBot.h>
 //Include da lib de Wifi do ESP8266
 #include <ESP8266WiFi.h>
 
 //PIN
 #define DHTTYPE DHT11
 #define DHTPIN D4
+
+// Seu token do telegram
+#define BOTtoken "1386192045:AAFMNQVjBkt-o--ip4KS1FLKn6oQeaThuis"  
+
+//Intervalo para obter novas mensagens
+#define BOT_SCAN_MESSAGE_INTERVAL 1000
+
+// Ultima vez que buscou mensagem
+long lastTimeScan; 
 
 //Definir o SSID da rede WiFi
 const char* ssid = "NOME_WIFI";
@@ -18,6 +29,7 @@ String apiKey = "b51f752b-3667-4f78-8306-8ce8e689410d";
 const char* server = "api.tago.io";
 int temp = 0;
 
+UniversalTelegramBot bot(BOTtoken, client);
 DHT dht(DHTPIN, DHTTYPE);
 WiFiClient client;
 
@@ -31,7 +43,9 @@ void setup() {
 
   Serial.print("Conectando na rede ");
   Serial.println(ssid);
-
+  
+  lastTimeScan = millis();
+  
   //Inicia o WiFi
   WiFi.begin(ssid, password);
 
@@ -48,6 +62,34 @@ void setup() {
   Serial.println(ssid);
   Serial.print("IP: ");
   Serial.println(WiFi.localIP());
+}
+
+// Trata as novas mensagens que chegaram
+void handleNewMessages(int numNewMessages) {
+   for (int i=0; i<numNewMessages; i++) {
+     
+     String chat_id = String(bot.messages[i].chat_id);
+     String text = bot.messages[i].text;
+     
+     // Pessoa que está enviando a mensagem
+     String from_name = bot.messages[i].from_name;    
+     if (from_name == "") from_name = "Convidado";
+     
+     if (text == "/start") {
+        String welcome = from_name + ", bem vindo ao BOT FIAP.\n";
+        welcome += "/env : saber a temperatura e umidade do ambiente \n";
+        bot.sendMessage(chat_id, welcome, "Markdown");
+    }
+     
+     if( text == "/env") {
+      float humidity = dht.readHumidity();
+      float temperature = FtoC(dht.readTemperature());
+      String message = "A temperatura é de " + String(temperature, 2) + " graus celsius.\n";
+      message += "A umidade relativa do ar é de " + String(humidity, 2)+ "%.\n\n";      
+      bot.sendMessage(chat_id, message, "Markdown");
+    }
+     
+  }
 }
 
 // Metodo responsavel por receber a temperatura e umidade e realizar uma requisicao do tipo POST para a api do Tago.io
@@ -100,6 +142,18 @@ float FtoC(float fahr){
 //Enviando os dados de 10 em 10 segundos
 void loop() {
   
+  if (millis() > lastTimeScan + BOT_SCAN_MESSAGE_INTERVAL)  {
+    // Serial.print("Checking messages - ");
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);  
+    // Serial.println(numNewMessages);
+    while(numNewMessages) {
+      // Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeScan = millis();
+  }
+    
   delay(readDelay);
 
    float temp = FtoC(dht.readTemperature());
@@ -107,7 +161,8 @@ void loop() {
 
    Serial.println(temp);
    Serial.println(umidade);
-       
+   
+   //Envia dados para o tago.io
    envia_dados(temp,umidade);
   
 }
